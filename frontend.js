2015520c099f51fef2fb7f87a44dbf061041dc24
Joselie Castaneda <jcing.jc@gmail.com>
2021-03-19 20:55:39 +0800

    working

--- frontend.js
+++ frontend.js
@@ -1,98 +1,49 @@
-(function () {
-  function observerStore(reducer, initialState) {
-    const store = {};
+function App() {
+  const [counter, setCounter] = React.useState(0);
+  const [counterTwo, setCounterTwo] = React.useState(0);
+  const [currentCounter, setCurrentCounter] = React.useState(true);
 
-    store.state = initialState;
-    store.listeners = [];
+  return (
+    <>
+      <h3 id="header">JavaScript C&C COUNTER</h3>
+      <h2 id="counter">{counter}</h2>
 
-    store.subscribe = function (listener) {
-      return store.listeners.push(listener);
-    };
+      <h2 id="counter2">{counterTwo}</h2>
 
-    store.dispatch = function (action) {
-      store.state = reducer(store.state, action);
-      store.listeners.forEach((listener) => listener(action));
-    };
+      <button
+        className="btn btn-primary"
+        id="increment"
+        style={{ marginRight: 20 }}
+        onClick={() =>
+          currentCounter
+            ? setCounter(counter + 1)
+            : setCounterTwo(counterTwo + 1)
+        }
+      >
+        INCREMENT
+      </button>
+      <button
+        className="btn btn-danger"
+        id="decrement"
+        onClick={() =>
+          currentCounter
+            ? setCounter(counter - 1)
+            : setCounterTwo(counterTwo - 1)
+        }
+      >
+        DECREMENT
+      </button>
+      <br />
+      <br />
+      <button
+        className="btn btn-info"
+        id="toggle"
+        onClick={() => setCurrentCounter(!currentCounter)}
+      >
+        {`TOGGLE ${currentCounter ? 1 : 2}`}
+      </button>
+    </>
+  );
+}
 
-    store.getState = function () {
-      return store.state;
-    };
-
-    return store;
-  }
-
-  const initialState = {
-    counter: 0,
-    counterTwo: 0,
-    currentCounter: 1,
-  };
-
-  function reducer(state = initialState, action) {
-    switch (action.type) {
-      case "INCREMENT":
-        return {
-          ...state,
-          [action.payload]: state[action.payload] + 1,
-        };
-      case "DECREMENT":
-        return {
-          ...state,
-          [action.payload]: state[action.payload] - 1,
-        };
-      case "TOGGLE_COUNTER":
-        return {
-          ...state,
-          currentCounter: action.payload,
-        };
-      default:
-        return state;
-    }
-  }
-
-  const myStore = observerStore(reducer, initialState);
-  const toggleHandler = document.getElementById("toggle");
-
-  function updateInterface() {
-    const countHandler = document.getElementById("counter");
-    const countTwoHandler = document.getElementById("counter2");
-    const { counter, counterTwo, currentCounter } = myStore.getState();
-
-    countTwoHandler.innerHTML = counterTwo;
-    countHandler.innerHTML = counter;
-    toggleHandler.innerHTML = `Toggle ${currentCounter}`;
-  }
-
-  function dummy(action) {
-    console.log("CURRENT ACTION DISPATCHED ********* ", action);
-  }
-
-  myStore.subscribe(updateInterface);
-  myStore.subscribe(dummy);
-
-  const incHandler = document.getElementById("increment");
-  const decHandler = document.getElementById("decrement");
-
-  incHandler.addEventListener("click", function () {
-    const { currentCounter } = myStore.getState();
-    myStore.dispatch({
-      type: "INCREMENT",
-      payload: currentCounter === 2 ? "counterTwo" : "counter",
-    });
-  });
-
-  decHandler.addEventListener("click", function () {
-    const { currentCounter } = myStore.getState();
-    myStore.dispatch({
-      type: "DECREMENT",
-      payload: currentCounter === 2 ? "counterTwo" : "counter",
-    });
-  });
-
-  toggleHandler.addEventListener("click", function () {
-    const { currentCounter } = myStore.getState();
-    myStore.dispatch({
-      type: "TOGGLE_COUNTER",
-      payload: currentCounter === 2 ? 1 : 2,
-    });
-  });
-})();
+ReactDOM.render(<App />, document.querySelector("#root"));
